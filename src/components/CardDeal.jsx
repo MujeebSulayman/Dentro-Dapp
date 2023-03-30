deb36cd59cdff19b416fd905e0a4838ff210cfc0
The Hemjay <79232253+MujeebSulayman@users.noreply.github.com>
2023-03-30 19:57:07 +0100

    Update CardDeal.jsx

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -11,7 +11,7 @@ const CardDeal = () => {
         </h2>
 
         <p
-          className={`${styles.paragraph2} w-full className='flex justity-center items-center py-[10px] px-[13px] bg-discount-gradient rounded-[10px] mt-9 text-start`}
+          className={`${styles.paragraph2} w-full className='flex justity-center items-center ml-[7] max-w-[470px]py-[10px] px-[13px] bg-discount-gradient rounded-[10px] mt-9 text-start`}
         >
           0xae5C84e1dfA1696949E67a2bBb2c9C91E4079007
         </p>
