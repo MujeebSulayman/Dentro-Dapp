e577923340b2678a4be2610cc941b0b499055cde
Hemjaytech <sulaymanmujeeb6@gmail.com>
2023-03-31 18:51:46 -0700

    main

--- src/pages/Roadmap.jsx
+++ src/pages/Roadmap.jsx
@@ -3,14 +3,14 @@ import styles, { layout } from '../style';
 
 const Roadmap = () => {
 	return (
-		<section className={` flex flex-col mt-11`}>
+		<section className={`${layout.section} m`}>
 			<div className={`${layout.sectionInfo} justify-center items-center`}>
 				<h2
 					className={`${styles.heading2} flex justify-center text-center m-9 items-center font-poppins `}
 				>
 					Roadmap
 				</h2>
-				<p className={`${styles.paragraph} text-left mr-50 ml-50`}>
+				<p className={`${styles.paragraph} text-left`}>
 					<p className='italic mb-6'>
 						[This Roadmap is temporary and it’s subject to review]
 					</p>
@@ -19,41 +19,61 @@ const Roadmap = () => {
 					project in the first quarter of the year 2021. This roadmap review
 					therefore itemizes the project accomplishments and what we look on to
 					bring into the crypto space as a proponent of planet re-greening and
-					advocate of independence for crypto-economy: 
-          <br />
-          <h2 className='text-dimWhite font-poppins text-40px'></h2> 1st Quarter 2021
-           Idea
-					Generation Constitution of Dentro Project Team. Project Planning and
-					Design 2nd Quarter 2021. Launching of social media platforms The
-					Launch of the Project website:- https://www.dentrocioin.com Dentrocoin
-					v1 Smart Contract Deployment Whitepaper Development Dentrocoin v1
-					Giveaway (Airdrop) 3rd Quarter 2021. Dentrocoin v2 Smart Contract
-					Deployment and Migration Launching of DENTRO/BNB Liquidity Pool on
-					DEX- Pancakeswap.finance The Lock of 55% Dentrocoin and LP Token with
-					DxSale 4th Quarter 2021. Listing of Dentrocoin on CEXes DentroBiz’s
-					incorporation of Dentlink International Listing of Dentrocoin on
-					Coinmarketcap Commissioning of Dentlink Mall, in West Africa. 1st
-					Quarter 2022. Launching of DentroPay; Official Decentralised Wallet of
-					Dentrocoin for Sending, Receiving and Storage of Cryptocurrencies Mini
-					Swap; Exchanging Cryptocurrencies to Dentrocoin on DentroPay Mini P2P;
-					Buying Dentrocoin with Fiat on DentroPay Website Redevelopment
-					Whitepaper Review Roadmap Review Influencers promotions DentroPay
-					Referral Campaign targeting 100,000 holders 2nd Quarter 2022.
-					Launching of Dentro-MS for Dentrocoin’s Merchants’ Adoption Media
-					Promotions Activation of Global Airtime Top-up on DentroPay More Major
-					CEX Listings of Dentrocoin 3rd Quarter 2022. Formal Launching of
-					Dentro Foundation Commencement of Re-greening Agenda Saving and
-					Staking on DentroPay DEX integration on DentroPay Dapp More Media
-					Coverage Promotions First Vesting of the Locked Tokens More CEX
-					Listings and IEOs First Burning of Unsold Tokens from IEOs 4th Quarter
-					2022. Extension of DentroBiz Tentacles Further Planting Efforts by
-					Dentro Foundation More Major Listing of Dentrocoin Reviewing the Swap
-					and P2P Features on DentroPay More Media Promotions Special Event
-					Estimate of Trees Planted in 2022 and Dentrocoin’s Burning NEXT:
-					Further Development of the Dentro Ecosystem and Replication of
-					Successes from DentroBiz and Dentro Foundations in Other Regions Note:
-					Subsequent phases will result from the successes recorded from the
-					unveiled phase
+					advocate of independence for crypto-economy:
+					<h2 className={`${styles.heading4} pt-9`}>1st Quarter 2021</h2>
+					<p className={`${styles.paragraph} m-9 pb-2`}>
+						<ul>
+							<li>Idea Generation</li>
+							<li>Constitution of Dentro Project Team.</li>
+							<li>Project Planning and Design 2nd Quarter 2021.</li>
+						</ul>
+						<br />
+					</p>
+					<h2 className={`${styles.heading4} pt-9`}>2nd Quarter 2021.</h2>
+					<p className={`${styles.paragraph} m-9`}>
+						<ul>
+							<li> Launching of social media platforms </li>
+							<li> The Launch of the Project</li>
+							<li> website:- https://www.dentrocioin.com </li>
+							<li> Dentrocoin v1 Smart Contract </li>
+							<li> Deployment Whitepaper</li>
+							<li> Development Dentrocoin v1 Giveaway (Airdrop) </li>
+						</ul>
+					</p>
+					<h2 className={`${styles.heading4} pt-9`}>3rd Quarter 2021.</h2>
+					<p className={`${styles.paragraph} m-9`}>
+						<ul>
+							<li>
+								Dentrocoin v2 Smart Contract Deployment and Migration Launching
+							</li>
+							<li>DENTRO/BNB Liquidity Pool on DEX- Pancakeswap.finance</li>
+							<li>The Lock 55% Dentrocoin and LP Token with DxSale </li>
+						</ul>
+					</p>
+					<h2 className={`${styles.heading4} pt-11 pb-8`}>4th Quarter 2021.</h2>
+					Listing of Dentrocoin on CEXes DentroBiz’s incorporation of Dentlink
+					International Listing of Dentrocoin on Coinmarketcap Commissioning of
+					Dentlink Mall, in West Africa. 1st Quarter 2022. Launching of
+					DentroPay; Official Decentralised Wallet of Dentrocoin for Sending,
+					Receiving and Storage of Cryptocurrencies Mini Swap; Exchanging
+					Cryptocurrencies to Dentrocoin on DentroPay Mini P2P; Buying
+					Dentrocoin with Fiat on DentroPay Website Redevelopment Whitepaper
+					Review Roadmap Review Influencers promotions DentroPay Referral
+					Campaign targeting 100,000 holders 2nd Quarter 2022. Launching of
+					Dentro-MS for Dentrocoin’s Merchants’ Adoption Media Promotions
+					Activation of Global Airtime Top-up on DentroPay More Major CEX
+					Listings of Dentrocoin 3rd Quarter 2022. Formal Launching of Dentro
+					Foundation Commencement of Re-greening Agenda Saving and Staking on
+					DentroPay DEX integration on DentroPay Dapp More Media Coverage
+					Promotions First Vesting of the Locked Tokens More CEX Listings and
+					IEOs First Burning of Unsold Tokens from IEOs 4th Quarter 2022.
+					Extension of DentroBiz Tentacles Further Planting Efforts by Dentro
+					Foundation More Major Listing of Dentrocoin Reviewing the Swap and P2P
+					Features on DentroPay More Media Promotions Special Event Estimate of
+					Trees Planted in 2022 and Dentrocoin’s Burning NEXT: Further
+					Development of the Dentro Ecosystem and Replication of Successes from
+					DentroBiz and Dentro Foundations in Other Regions Note: Subsequent
+					phases will result from the successes recorded from the unveiled phase
 				</p>
 			</div>
 		</section>
