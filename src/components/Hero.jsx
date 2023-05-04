959d6c38b5f7b0a054021fa7da6f56a30e78cb18
Hemjaytech <sulaymanmujeeb6@gmail.com>
2023-05-04 16:59:13 -0700

    main

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,7 +13,7 @@ const Hero = () => {
         className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}
       >
         <div className='flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-9'>
-          <img src={discount} alt='discount' className='h-[32px] w-[32px]' />
+          <img src={discount} alt='discount' className='h-[32px] w-[40px]' />
           <p className={`${styles.paragraph} ml-2 `}>
             We owe it to humanity to save this planet.
           </p>
