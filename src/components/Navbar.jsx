4fffffbf3bf7d3a56363640805e3f0c3ba59b0d4
Hemjaytech <sulaymanmujeeb6@gmail.com>
2023-05-28 04:22:29 -0700

    menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -43,7 +43,7 @@ const Navbar = () => {
 				<div
 					className={`${
 						toggle ? 'flex' : 'hidden'
-					} p-6 backdrop-blur-sm backdrop-brightness-150 md:backdrop-filter-none absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
+					} bg-black/10 p-6 backdrop-blur-sm backdrop-brightness-150 md:backdrop-filter-none absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
 				>
 					<ul className='list-none flex-col justify-end items-center flex-1'>
 						{navLinks.map((nav, index) => (
