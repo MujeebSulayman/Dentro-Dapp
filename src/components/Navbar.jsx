1ef6f6d81d9280d4efec59e5aac5eac4ad53ac07
Hemjaytech <sulaymanmujeeb6@gmail.com>
2023-05-31 11:14:54 -0700

    gg

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -37,7 +37,7 @@ const Navbar = () => {
 				<img
 					src={toggle ? close : menu}
 					alt='menu'
-					className='w-28px h-28px object-contain cursor-pointer'
+					className='w-26px h-26px object-contain cursor-pointer'
 					onClick={() => setToggle((prev) => !prev)}
 				/>
 				<div
