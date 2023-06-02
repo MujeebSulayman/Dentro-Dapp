6267c624ad82ffba8c315c96ddb11b759f185229
Hemjaytech <sulaymanmujeeb6@gmail.com>
2023-06-02 00:24:57 -0700

    icon

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -16,12 +16,12 @@ const Button = ({ styles }) => {
       </button>
       <button
         type='button'
-        className={`py-4 px-6 flex-1 justify-between items-center bg-white font-poppins font-normal text-[18px] text-white outline-none ${styles} rounded-[5px] mb-[50px] circle-btn`}
+        className={`py-4 px-6 flex-1 flex justify-center items-center bg-white font-poppins font-normal text-[18px] text-white outline-none ${styles} rounded-[5px] mb-[50px] circle-btn`}
       >
         <a href="https://people-utility-token.gitbook.io/peoples-utility-token/" target="_blank" rel="noopener noreferrer">
 
           Whitepaper
-        </a>
+        </a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M5 12h13M12 5l7 7-7 7" /></svg>
       </button>
     </div>
   );
